ce0b0e6fbdaf29086953a58e11e193177c31729b
async-devil <async.devil@gmail.com>
2021-11-16 20:02:51 +0200

    rearrange: delete users module

--- src/users/dto/create-user.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from "@nestjs/swagger";
-
-export class CreateUserDto {
-	@ApiProperty({ example: "test@email.com" })
-	readonly email: string;
-
-	@ApiProperty({ example: "Password5@1" })
-	readonly password: string;
-}
