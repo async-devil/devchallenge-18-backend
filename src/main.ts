851357de6cfe4ff28d783345559bf3f9befba27d
async-devil <async.devil@gmail.com>
2021-11-17 19:59:31 +0200

    fix: update port

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from "@nestjs/core";
 
 import { AppModule } from "./app.module";
 
-const port = 3000;
+const port = 8080;
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
