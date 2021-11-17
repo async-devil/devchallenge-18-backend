c79f6f7f4df4e78e5babad224a848d66dd2f107d
async-devil <async.devil@gmail.com>
2021-11-17 19:59:03 +0200

    feat: add cutter service to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common";
 
 import { AppController } from "./app.controller";
+import { CutterService } from "./cutter/cutter.service";
 import { ErrorsService } from "./errors.service";
 import { ValidationService } from "./validation/validation.service";
 
 @Module({
 	controllers: [AppController],
-	providers: [ErrorsService, ValidationService],
+	providers: [ErrorsService, ValidationService, CutterService],
 })
 export class AppModule {}
