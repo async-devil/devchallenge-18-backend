476746857995af8bb12306969490ad8ef72c034c
async-devil <async.devil@gmail.com>
2021-11-17 22:45:22 +0200

    rearrange: delete dist folder

--- dist/dto/CutBox.dto.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CutBoxDto = void 0;
-class CutBoxDto {
-}
-exports.CutBoxDto = CutBoxDto;
-//# sourceMappingURL=CutBox.dto.js.map
--- dist/errors.service.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class HttpError {
-    readonly succsess: boolean;
-    readonly error: string;
-}
-export declare class ErrorsService {
-    private readonly logger;
-    throwInvalidInputError(message: string): void;
-    throwInvalidSizeError(): void;
-}
